2c2169c61f08dc8ef2919d3d340679f1454572c0
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-11-13 20:00:43 -0700

    Updated

--- source/app/containers.basic/BasicContainerSpecs.cs
+++ source/app/containers.basic/BasicContainerSpecs.cs
@@ -4,6 +4,7 @@ using app.containers.core;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
+using Rhino.Mocks.Constraints;
 
 namespace app.containers.basic
 {
@@ -17,28 +18,67 @@ namespace app.containers.basic
 
     public class when_getting_a_dependency : concern
     {
-      Establish c = () =>
+      public class and_the_factory_for_the_dependency_can_create_it_successfully
       {
-        command = fake.an<IDbCommand>();
-        factory = fake.an<ICreateADependency>();
-        factories = depends.on<IGetFactoriesForDependencies>();
+        Establish c = () =>
+        {
+          command = fake.an<IDbCommand>();
+          factory = fake.an<ICreateADependency>();
+          factories = depends.on<IGetFactoriesForDependencies>();
 
-        factories.setup(x => x.get_factory_that_can_create(typeof(IDbCommand)))
-          .Return(factory);
+          factories.setup(x => x.get_factory_that_can_create(typeof(IDbCommand)))
+            .Return(factory);
 
-        factory.setup(x => x.create()).Return(command);
-      };
+          factory.setup(x => x.create()).Return(command);
+        };
 
-      Because b = () =>
-        result = sut.an<IDbCommand>();
+        Because b = () =>
+          result = sut.an<IDbCommand>();
 
-      It returns_the_item_created_by_the_factory_for_that_dependency = () =>
-        result.ShouldEqual(command);
+        It returns_the_item_created_by_the_factory_for_that_dependency = () =>
+          result.ShouldEqual(command);
 
-      static IDbCommand result;
-      static IDbCommand command;
-      static ICreateADependency factory;
-      static IGetFactoriesForDependencies factories;
+        static IDbCommand result;
+        static IDbCommand command;
+        static ICreateADependency factory;
+        static IGetFactoriesForDependencies factories;
+      }
+
+      public class and_the_factory_for_the_dependency_fails_to_create_the_dependency
+      {
+        Establish c = () =>
+        {
+          original_exception = new Exception();
+          custom_exception = new Exception();
+
+          factory = fake.an<ICreateADependency>();
+          factories = depends.on<IGetFactoriesForDependencies>();
+
+          depends.on<ICreateAnExceptionWhenTheDependencyCantBeCreated>((type, exception) =>
+          {
+            type.ShouldEqual(typeof(IDbCommand));
+            exception.ShouldEqual(original_exception);
+            return custom_exception;
+          });
+
+          factories.setup(x => x.get_factory_that_can_create(typeof(IDbCommand)))
+            .Return(factory);
+
+          factory.setup(x => x.create()).Throw(original_exception);
+        };
+
+        Because b = () =>
+          spec.catch_exception(() => sut.an<IDbCommand>());
+
+        It throws_the_exception_created_by_the_dependency_creation_exception_factory = () =>
+          spec.exception_thrown.ShouldEqual(custom_exception);
+
+        static ICreateADependency factory;
+        static IGetFactoriesForDependencies factories;
+        static Exception custom_exception;
+        static Exception original_exception;
+      }
+
     }
   }
 
