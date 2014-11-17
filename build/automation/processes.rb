7797ad2309be7444b742046b1adb9ae760e0aff5
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-11-16 21:30:48 -0700

    Updated

--- build/automation/processes.rb
+++ build/automation/processes.rb
@@ -5,8 +5,13 @@ module Automation
     desc 'kill_runner_processes', 'kill all the runner processes'
     def kill_runner_processes
       settings.runner_processes.each do |process|
-        system("taskkill /F /IM #{process}.exe")
+        kill process
       end
+    end
+
+    desc 'kill', 'kills a process'
+    def kill(process)
+      system("taskkill /F /IM #{process}.exe")
     end
   end
 end
